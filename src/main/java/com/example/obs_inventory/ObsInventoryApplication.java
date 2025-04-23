a3bf9d31b0f5cea23043a258d684115e8150f144
jemmiepriadi <jemmyepriadi@gmail.com>
2025-04-24 00:50:03 +0700

    first commit

---
+++ src/main/java/com/example/obs_inventory/ObsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.obs_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsInventoryApplication.class, args);
+	}
+
+}
